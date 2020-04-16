cd07d793e9f25fbed2f8b59ac5480a00959348f7
andre6e <andrea-fedele@hotmail.it>
2020-04-16 19:20:20 +0200

    -Aggiunto zoomout -Modifica scroll to element adesso considera anche l altezza dell elemento -Modificata funzione di click al pulsante next (dava errore probabilmente perche con zoomout viene nascosta la possibilita di zoommare)

--- browser_navigator.py
+++ browser_navigator.py
@@ -8,9 +8,19 @@ config.read('config.ini')
 LOGIN_URL = "https://www.linkedin.com/uas/login"
 
 class BrowserNavigator:
+    # ZOOMERS
+    def zoom_out_browser(self):
+        self.browser.execute_script("document.body.style.zoom = '65%'")
+
+    def wait_and_zoom_out(self):
+        time.sleep(1)
+        self.zoom_out_browser()
+        time.sleep(1)
+
     # HELPERS
 
-    def save_users_to_csv(self):
+    def export_users_to_csv(self):
+        print("Exporting users list to csv")
         with open('users.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(self.users_list)
@@ -19,6 +29,7 @@ class BrowserNavigator:
         # Url copiato e incollato fa filtering su country Italy e basta
         people_url = 'https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22it%3A0%22%5D&origin=FACETED_SEARCH'
         self.browser.get(people_url)
+        self.wait_and_zoom_out()
 
     def find_element(self, css_selector):
         element = self.browser.find_element_by_class_name(css_selector)
@@ -63,16 +74,22 @@ class BrowserNavigator:
     def go_to_next_page_by_clicking(self):
         next_page_btn_cname = 'artdeco-pagination__button--next'
         next_page_btn = self.browser.find_element_by_class_name(next_page_btn_cname)
-        next_page_btn.click()
+        # next_page_btn.click()
+        self.browser.execute_script("arguments[0].click();", next_page_btn)
 
     # SCROLLERS
 
     def scroll_page(self):
         self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-    def scroll_to_element_height(self, height):
-        print("scrolling to %s element height" % (height))
-        self.browser.execute_script("window.scrollTo(0, %s);" % (height))
+    def scroll_to_element_height(self, elem):
+        # Distanza da top pagina
+        elem_scroll_height = elem.get_attribute("offsetTop")
+        # Altezza elemento
+        elem_offset_height = elem.get_attribute("offsetHeight")
+        to_scroll = str(int(elem_scroll_height) + int(elem_offset_height) - (int(elem_offset_height) / 2) )
+        print("scrolling to %s element height" % (to_scroll))
+        self.browser.execute_script("window.scrollTo(0, %s);" % (to_scroll))
 
     def scroll_page_to_end(self):
         sleep_time = self.SLEEP_TIME
@@ -83,6 +100,12 @@ class BrowserNavigator:
             time.sleep(sleep_time)
 
         print("Finished scrolling the page.")
+
+    def scroll_to_pagination(self):
+        next_page_btn_cname = 'artdeco-pagination__button--next'
+        next_page_btn = self.browser.find_element_by_class_name(next_page_btn_cname)
+        self.scroll_to_element_height(next_page_btn)
+        self.wait_to_find_element_by_css(next_page_btn_cname)
     
     # MAIN
     def retreive_users_url(self):
@@ -94,16 +117,18 @@ class BrowserNavigator:
                 
             self.scrape_result_page()
             self.scroll_page_to_end()
-            
+            self.scroll_to_pagination()
             self.go_to_next_page_by_clicking()
+
             time.sleep(2)
             
             if len(self.users_list) >= self.USERS_TO_SCRAPE:
                 break
 
-        self.save_users_to_csv()
+        self.export_users_to_csv()
 
     def scrape_result_page(self):
+        self.wait_and_zoom_out()
         results_ul_cname = 'search-results__list'
     
         # aspetto trovi la lista di risultati
@@ -114,8 +139,7 @@ class BrowserNavigator:
         search_info_cname = 'search-result__info'
         for option in options:
             # Scrollo all'altezza della singola persona
-            option_scroll_height = option.get_attribute("offsetTop")
-            self.scroll_to_element_height(option_scroll_height)
+            self.scroll_to_element_height(option)
             # Aspetto che le informazioni vengano renderizzate
             self.wait_to_find_element_by_css(search_info_cname)
             
@@ -147,7 +171,7 @@ class BrowserNavigator:
         elementID.submit()
 
         while self.browser.current_url == LOGIN_URL:
-            time.sleep(self.SLEEP_TIME)
+            time.sleep(3)
 
         print("Logged")
 
